b3fc651c48bf3ea294db7c93978b1c50369af417
montheramer <monther.amer48@gmail.com>
2021-08-11 21:18:30 +0300

    add react routes and basic pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+
 const App = () => {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/movie/:id" render={() => <Movie />} exact={true} />
+          <Route path="/" render={() => <Home />} exact={false} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
