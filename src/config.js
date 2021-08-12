08f08e7c92a399d271d8da30e4852d60ddb60c72
montheramer <monther.amer48@gmail.com>
2021-08-12 04:10:18 +0300

    deploy to gh-pages

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-export const MOVIES_BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const MOVIES_BASE_URL = '//api.themoviedb.org/3/movie/';
 
-export const SEARCH_BASE_URL = 'https://api.themoviedb.org/3/search/movie/';
+export const SEARCH_BASE_URL = '//api.themoviedb.org/3/search/movie/';
 
-export const IMAGES_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+export const IMAGES_BASE_URL = '//image.tmdb.org/t/p/w500';
 
 export const API_KEY = 'f1b8d194dba80bd94de376afc7c19763';
